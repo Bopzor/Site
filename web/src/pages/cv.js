810178c053a6713a738757cf4508c5db414dcaf1
Bopzor <violaine.ballandin@gmail.com>
2018-09-02 21:48:19 +0200

    change path

--- web/src/pages/cv.js
+++ web/src/pages/cv.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CV = () => {
   return (
-    <object data="http://localhost/cv.pdf" type="application/pdf" width="750px" height="750px">
+    <object data="http://bopzor.me/images/cv-bopzor.me.pdf" type="application/pdf" width="750px" height="750px">
     </object>
   );
 }
