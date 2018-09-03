8a0ebd749c7a5c8d9434a03cab0e072bb2e46d7d
Bopzor <violaine.ballandin@gmail.com>
2018-09-04 01:58:40 +0200

    add link if no display

--- web/src/pages/cv.js
+++ web/src/pages/cv.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const CV = () => {
   return (
-    <object data="http://bopzor.me/images/cv-bopzor.me.pdf" type="application/pdf" width="750px" height="750px">
+    <object
+      data="https://bopzor.me/images/cv-bopzor.me.pdf"
+      type="application/pdf"
+      width="750px"
+      height="750px"
+    >
+      <a href="http://bopzor.me/images/cv-bopzor.me.pdf">Voir le CV</a>
     </object>
   );
 }
