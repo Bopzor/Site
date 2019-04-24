330aec1856e81293025fbe315f99371d802ceb7c
Bopzor <violaine.ballandin@gmail.com>
2019-04-24 14:56:01 +0200

    change path of images

--- web/src/pages/cv.js
+++ web/src/pages/cv.js
@@ -4,14 +4,14 @@ import '../css/style.css';
 const CV = () => {
   return (
     <object
-      data="https://bopzor.me/images/cv-bopzor.me.pdf"
+      data="../images/cv-bopzor.me.pdf"
       type="application/pdf"
       width="750px"
       height="750px"
     >
-      <a href="http://bopzor.me/images/cv-bopzor.me.pdf">
+      <a href="../images/cv-bopzor.me.pdf">
         <page className="cv">
-          <img src="http://bopzor.me/cv-bopzor.me.png" className="cv-img" alt="CV"/>
+          <img src="../cv-bopzor.me.png" className="cv-img" alt="CV"/>
         </page>
       </a>
     </object>
