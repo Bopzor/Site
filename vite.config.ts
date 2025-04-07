211df0666e8f4a04341e603aefcfcd5c084deab7
Bopzor <26028196+Bopzor@users.noreply.github.com>
2025-04-07 12:31:14 +0200

    feat(redirection): redirect 404 to index.html for SPA handling in github page

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,36 @@
-import { defineConfig } from "vite";
+import fs from "node:fs/promises";
+import path from "node:path";
+
+import { defineConfig, Plugin } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+    copyFile({ src: "index.html", dest: "404.html" }),
+  ],
 });
+
+type CopyFileOptions = {
+  src: string;
+  dest: string;
+};
+
+function copyFile({ src, dest }: CopyFileOptions): Plugin {
+  let dist = "";
+
+  return {
+    name: "outputFile",
+    configResolved(config) {
+      dist = path.resolve(config.root, config.build.outDir);
+    },
+    async closeBundle() {
+      await fs.copyFile(path.join(dist, src), path.join(dist, dest));
+    },
+  };
+}
